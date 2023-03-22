ca5dce7eb1f6150809000dc9e53157b27d37e1f3
Andres2908 <saavedratrejo24@gmail.com>
2023-03-21 23:04:23 -0600

    componente de video estatico

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function App({ src, width, height }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container-md mt-1">
+        <video
+          id="demo"
+          controls
+          className="video-js vjs-default-skin"
+          loop
+          width={width}
         >
-          Learn React
-        </a>
-      </header>
+          <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
+          <source src="http://vjs.zencdn.net/v/oceans.webm" type="video/webm" />
+        </video>
+
+        <div className="contenedor">
+          <div className="input-group mb-3 mt-2">
+            <input
+              className="form-control"
+              placeholder="Escribe una pequeña nota"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+          </div>
+          <div className="align-btn">
+            <button type="button" className="btn btn-outline-secondary">
+              Cancelar
+            </button>
+            <button type="button" className="btn btn-dark" id="save">
+              Guardar
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
